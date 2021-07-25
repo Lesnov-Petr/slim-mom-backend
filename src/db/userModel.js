48ddcded28842fda2cfab10ac9850ffd4d9cfde2
Iryna Protsenko <irene.protsenko@gmail.com>
2021-07-25 13:12:57 +0300

    add userModel, env-example

---
+++ src/db/userModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for user']
+  },
+  // email: {
+  //  type: String,
+    // required: [true, 'Set name for user']
+  // },
+  login: {
+    type: String,
+    required: [true, 'Set login for user'],
+    unique:true
+  },
+  password: {
+    type: String,
+    required: [true, 'Set password for user']
+  },
+
+  userInfo: {
+    height: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: true
+    },
+    age: {
+      type: Number,
+      required: true
+    },
+    desiredWeight: {
+      type: Number,
+      required: true
+    },
+    bloodGroup: {
+      type: Number,
+      required: true,
+      //type is Number, and default meaning -do we need?
+    }
+  }
+})
+
+userSchema.pre('save', async function () {
+  if (this.isNew) {
+    this.password = bcrypt.hash(this.password, 10)
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+  User
+}

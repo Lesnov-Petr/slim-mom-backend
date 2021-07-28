8459bfc0efb019204e820d55d04189ef5ae4aeca
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-07-28 13:44:18 +0800

    Update productsRouter.js

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -17,12 +17,8 @@ router.use(authMiddleware);
 router.get("/search", asyncWrapper(searchProductsController));
 router.get("/recommendation", asyncWrapper(publicRecommendationController));
 router.post("/recommendation", asyncWrapper(privateRecommendationController));
-// 8 энд-поинт на добавление съеденного продукта в конкретный день
-router.post("/eaten/:userId", asyncWrapper(addEatenProductsController));
-
-// 9 энд-поинт на удаление съеденного продукта в конкретный день
+router.post("/eaten", asyncWrapper(addEatenProductsController));
 router.delete("/eaten/:id", asyncWrapper(deleteEatenProductsController));
-// 10 энд-поинт на получение всей информации по конкретному дню
 router.get("/eaten", asyncWrapper(getEatenProductsController));
 
 module.exports = { productsRouter: router };
